e1d86adecacc69b070ae9b8ef4672a5010b3f2e4
孙兵 <carl900@live.com>
2018-02-09 13:36:10 +0800

    sync

---
+++ Vue/User01/src/components/nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <a href="/">首页1</a> &nbsp; | &nbsp;
+      <a href="/test.html">测试2</a> &nbsp; | &nbsp;
+      <a href="/account.html">账户3</a> &nbsp; | &nbsp;
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a{color: red;}
+</style>
