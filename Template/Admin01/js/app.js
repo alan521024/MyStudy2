57004f5f345cba2fad45a1567c5d6f8290728748
孙兵 <carl900@live.com>
2018-02-24 17:25:08 +0800

    后台界面

---
+++ Template/Admin01/js/app.js
@@ -0,0 +1,26 @@
+﻿; !(function (win) {
+
+    " use strict ";
+
+    var root = win;
+
+    var app = function (obj) {
+        if (obj instanceof app) return obj;
+        if (!(this instanceof app)) return new app(obj);
+        //this._wrapped = obj;
+    };
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = app;
+        }
+        exports.app = app;
+    }
+    else {
+        root.app = app;
+    }
+
+    app.VERSION = '1.0.0';
+
+
+})(window);
